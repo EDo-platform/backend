73175e7f3300fe85cef38bc6817b1286a95b2b76
LYJ0304 <omlmg0304@naver.com>
2025-09-30 19:14:25 +0900

    feat: 허용 도메인 추가

--- src/main/java/com/edo/backend/web/WebConfig.java
+++ src/main/java/com/edo/backend/web/WebConfig.java
@@ -13,7 +13,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**") // 모든 API 경로 허용
-                        .allowedOrigins("https://e-do-education.vercel.app") // 특정 도메인만 허용
+                        .allowedOrigins("https://e-do-education.vercel.app", "https://e-do-education-one.vercel.app") // 특정 도메인만 허용
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true); // 쿠키/인증정보 포함 허용 (필요시)
